a48351b0f50f7ddbb83cbd9f31e72e0553bfff41
NelloKudo <marshnelloosu@gmail.com>
2025-07-21 19:22:05 +0200

    bump/spritz: added WuWa WebView fix, bump to spritz-250721

--- patches/apply.sh
+++ patches/apply.sh
@@ -22,6 +22,7 @@ pushd "$here"/../wine || exit 1
     patch_cmd "$here"/spritz/0002-HACK-kernelbase-Terminate-Genshin-Impact-if-ExitProc.patch
     patch_cmd "$here"/spritz/0003-HACK-winex11-make-WM-focus-window-with-WINE_USE_TAKE.patch
     patch_cmd "$here"/spritz/0004-cachyos-ntsync.patch
+    patch_cmd "$here"/spritz/0005-HACK-winex11-Disable-window-opacity-for-Wuthering-Wa.patch
 popd || exit 1
 
 pushd "$here"/../vkd3d-proton || exit 1
