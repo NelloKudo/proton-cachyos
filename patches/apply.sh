ec411f47d2eeb8937c87f3499f47a90f5ec270ca
NelloKudo <marshnelloosu@gmail.com>
2025-08-14 17:10:02 +0200

    spritz: added Spritz patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -17,6 +17,13 @@ pushd "$here"/../glslang || exit 1
     patch_cmd "$here"/glslang/glslang-renderdoc-1.36-gcc15-fix.patch
 popd || exit 1
 
+pushd "$here"/../wine || exit 1
+    patch_cmd "$here"/spritz/0001-HACK-ws2_32-Block-first-internet-connection-for-some.patch
+    patch_cmd "$here"/spritz/0002-HACK-kernelbase-Terminate-Genshin-Impact-if-ExitProc.patch
+    patch_cmd "$here"/spritz/0003-HACK-winex11-make-WM-focus-window-with-WINE_USE_TAKE.patch
+    patch_cmd "$here"/spritz/0004-HACK-winex11-Disable-window-opacity-for-Wuthering-Wa.patch
+popd || exit 1
+
 pushd "$here"/../vkd3d-proton || exit 1
 popd || exit 1
 
