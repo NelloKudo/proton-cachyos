33c376ff7884b17246641fb9043ee03b604079a6
NelloKudo <marshnelloosu@gmail.com>
2025-09-27 18:10:10 +0200

    patches: added Spritz-Wine patches
    See: https://github.com/NelloKudo/Wine-Builds

--- patches/apply.sh
+++ patches/apply.sh
@@ -12,3 +12,11 @@ pushd "$here"/../glslang || exit 1
     patch_cmd "$here"/glslang/glslang-renderdoc-1.36-gcc15-fix.patch
 popd || exit 1
 
+pushd "$here"/../wine || exit 1
+    patch_cmd "$here"/spritz/0001-HACK-ws2_32-Block-first-internet-connection-for-some.patch
+    patch_cmd "$here"/spritz/0002-HACK-kernelbase-Terminate-Genshin-Impact-if-ExitProc.patch
+    patch_cmd "$here"/spritz/0003-HACK-winex11-make-WM-focus-window-with-WINE_USE_TAKE.patch
+    patch_cmd "$here"/spritz/0004-HACK-winex11-Disable-window-opacity-for-Wuthering-Wa.patch
+    patch_cmd "$here"/spritz/0005-ntdll-set-WINE_ENABLE_STEAM_STUB-for-GI.patch
+    patch_cmd "$here"/spritz/0006-backports-ntdll-Also-trap-syscalls-in-the-top-down-reserved-ar.patch
+popd || exit 1
